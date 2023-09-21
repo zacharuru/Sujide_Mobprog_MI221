fca19e3b3d08a1d7a204dc1db5f5f9f51ae0a5a2
zacharuru <145649358+zacharuru@users.noreply.github.com>
2023-09-21 02:17:36 -0500

    MainActivity.java

---
+++ Lab Exercise 4/MainActivity.java
@@ -0,0 +1,63 @@
+package com.example.labex4sujidezach;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText number1EditText;
+    private EditText number2EditText;
+    private TextView sumTextView;
+    private TextView subtractionTextView;
+    private TextView productTextView;
+    private TextView quotientTextView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        number1EditText = findViewById(R.id.number1EditText);
+        number2EditText = findViewById(R.id.number2EditText);
+        sumTextView = findViewById(R.id.sumTextView);
+        subtractionTextView = findViewById(R.id.subtractionTextView);
+        productTextView = findViewById(R.id.productTextView);
+        quotientTextView = findViewById(R.id.quotientTextView);
+
+        Button equalsButton = findViewById(R.id.equalsButton);
+        equalsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                calculateResults();
+            }
+        });
+    }
+
+    private void calculateResults() {
+        try {
+            double num1 = Double.parseDouble(number1EditText.getText().toString());
+            double num2 = Double.parseDouble(number2EditText.getText().toString());
+
+            double sum = num1 + num2;
+            double subtraction = num1 - num2;
+            double product = num1 * num2;
+            double quotient = num1 / num2;
+
+            sumTextView.setText("" + sum);
+            subtractionTextView.setText("" + subtraction);
+            productTextView.setText("" + product);
+            quotientTextView.setText("" + quotient);
+        } catch (NumberFormatException e) {
+            // Handle invalid input here
+            sumTextView.setText("");
+            subtractionTextView.setText("");
+            productTextView.setText("");
+            quotientTextView.setText("");
+        }
+    }
+}
